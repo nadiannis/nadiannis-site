6a149b31b54e0d4e5eb91d6c7f464f55f4893109
nadiannis <annisanadianeyla@gmail.com>
2021-07-31 08:44:02 +0700

    feat: create light/dark mode toggle

--- components/layouts/SingleSectionLayout.js
+++ components/layouts/SingleSectionLayout.js
@@ -1,9 +1,13 @@
+import { useTheme } from '../../store/themeContext';
+
 import MinimalHeader from './MinimalHeader';
 import MinimalFooter from './MinimalFooter';
 
 export default function SingleSectionLayout({ children }) {
+  const { theme } = useTheme();
+
   return (
-    <div className="bg-baseColor h-screen flex flex-col">
+    <div className={`bg-baseColor h-screen flex flex-col ${theme}`}>
       <MinimalHeader />
       <main className="flex-auto">{children}</main>
       <MinimalFooter />
