6da5a582ac52a372007a14a336594bd757928cba
nadiannis <annisanadianeyla@gmail.com>
2021-08-18 19:49:02 +0700

    feat: add page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,30 @@
+import { useEffect } from 'react';
+
+import NProgress from 'nprogress';
+
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 import { ThemeContextProvider } from '../store/themeContext';
 
-function MyApp({ Component, pageProps }) {
+const progress = NProgress.configure({ showSpinner: false });
+
+function MyApp({ Component, pageProps, router }) {
   const getLayout = Component.getLayout || ((page) => page);
+
+  useEffect(() => {
+    const handleStart = () => progress.start();
+    const handleStop = () => progress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
 
   return (
     <ThemeContextProvider>
