748a4e57fd294a0b69d6cff7a79182939a25be8e
nadiannis <annisanadianeyla@gmail.com>
2022-11-14 23:40:12 +0700

    feat: add cova project to projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 
+import projects from '@/data/projects.json';
 import Layout from '@/components/layouts/Layout';
 import Container from '@/components/ui/Container';
+import ProjectList from '@/components/projects/ProjectList';
 
 export default function ProjectsPage() {
   return (
@@ -14,21 +16,12 @@ export default function ProjectsPage() {
         <title>Projects | Annisa Nadia</title>
       </Head>
 
-      <section className="text-main py-12 w-full">
+      <section className="text-main py-8 sm:py-12 w-full">
         <Container>
-          <div className="text-center">
-            <h1 className="text-2xl md:text-3xl text-center inline-block border-b border-gray-300 dark:border-gray-600">
-              Projects
-            </h1>
-          </div>
-          <div className="py-12">
-            <h2 className="text-6xl md:text-7xl lg:text-8xl uppercase text-center text-secondaryInverted mb-6 tracking-widest">
-              Coming Soon
-            </h2>
-            <p className="text-sm md:text-base max-w-screen-md mx-auto text-center">
-              This page is currently being worked on. Please come back later.
-            </p>
-          </div>
+          <h1 className="text-2xl md:text-3xl inline-block border-b border-gray-300 dark:border-gray-600 mb-7">
+            Projects
+          </h1>
+          <ProjectList projects={projects} />
         </Container>
       </section>
     </>
