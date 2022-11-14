2c9c1f9b22985fcdaa449fc2780a715a9a6334a4
nadiannis <annisanadianeyla@gmail.com>
2022-11-14 23:36:55 +0700

    feat: make the Button component has option to be opened in the new tab

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,11 +1,32 @@
 import Link from 'next/link';
 
-export default function Button({ children, link, onClick, className }) {
+export default function Button({
+  children,
+  link,
+  onClick,
+  className,
+  isOpenInNewTab = false,
+}) {
   if (!link) {
     return (
       <button type="button" onClick={onClick} className={`btn ${className}`}>
         {children}
       </button>
+    );
+  }
+
+  if (isOpenInNewTab) {
+    return (
+      <Link href={link}>
+        <a
+          href={link}
+          className={`btn ${className}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {children}
+        </a>
+      </Link>
     );
   }
 
